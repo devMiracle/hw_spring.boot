5909895a10b9628a9277478d114d73a777cf26ee
Miracle <51058450+devMiracle@users.noreply.github.com>
2021-03-14 16:37:00 +0200

    Initial commit

---
+++ brocker/src/test/java/net/miracle/brocker/BrockerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.miracle.brocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
